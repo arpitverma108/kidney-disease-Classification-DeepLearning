3b74b538d71769e19167853557deeeb50c3d97a1
Arpit Verma <arpitv0710@gmail.com>
2025-11-08 15:05:19 +0530

    prepare base model done

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.kidneyClassifier import logger
 from src.kidneyClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from src.kidneyClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 
@@ -10,6 +10,20 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
